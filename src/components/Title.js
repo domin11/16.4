92e96729ba55351ef980ca803f26aa6b7ac35790
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-18 14:46:17 +0200

    Add TodoList.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Title = props =><div><h1>{"tytuł aplikacji"}</h1></div>
+const Title = props =><div><h1>{"Tasks to do:"}</h1></div>
 
 export default Title;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
 import Title from '../components/Title.js';
+import TodoList from '../components/TodoList.js';
 
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            data: []
+            data: [{
+              id: 1,
+                  text: 'clean room'
+              }, {
+              id: 2,
+                  text: 'wash the dishes'
+              }, {
+              id: 3,
+                  text: 'feed my cat'
+            }]
         };
     }
-    
+
 	addTodo(val){
         const todo = {
             text: val,
@@ -19,16 +29,17 @@ class App extends React.Component {
         const data = [...this.state.data, todo];
         this.setState({data});
     }
-	
+
 	removeTodo(id) {
 		const remainder = this.state.data.filter(todo => todo.id !== id);
 		this.setState({data: remainder});
 	}
-	
+
 	render() {
 		return (
 			<div className={style.TodoApp}>
 				<Title />
+        <TodoList list={this.state.data} remove={this.removeTodo.bind(this)} />
 			</div>
 		);
 	}
